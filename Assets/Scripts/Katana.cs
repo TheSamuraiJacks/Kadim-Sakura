fda545e948e3e881df5457fdf3a8af03466ce0e8
adil umut tuncer <adilutuncer@gmail.com>
2025-12-13 22:10:58 +0300

    dnm

--- Assets/Scripts/Katana.cs
+++ Assets/Scripts/Katana.cs
@@ -5,15 +5,44 @@ using UnityEngine;
 public class Katana : MonoBehaviour
 {
     public string TagName;
+    public AudioClip hitSound; // Ses dosyası
+    private AudioSource audioSource;
+
+    void Start()
+    {
+        // AudioSource bileşenini al veya ekle
+        audioSource = GetComponent<AudioSource>();
+        if (audioSource == null)
+        {
+            audioSource = gameObject.AddComponent<AudioSource>();
+        }
+    }
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.tag == TagName)
         {
-            if(other.gameObject.GetComponent<Enemy>() != null)
-            other.gameObject.GetComponent<Enemy>().TakeDamage(10);
+            bool isEnemy = false;
 
+            // Düşman kontrolü
+            if(other.gameObject.GetComponent<Enemy>() != null)
+            {
+                other.gameObject.GetComponent<Enemy>().TakeDamage(10);
+                isEnemy = true; // Sadece düşmansa true yap
+            }
+
+            // Diğer kırılabilir objeler vs.
             if(other.gameObject.GetComponent<AttackController>() != null)
-            other.gameObject.GetComponent<AttackController>().TakeDamage(10);
+            {
+                other.gameObject.GetComponent<AttackController>().TakeDamage(10);
+                // Burası isEnemy'yi true YAPMAZ, böylece ses çalmaz
+            }
+
+            // Sadece düşmana vurduysak ses çal
+            if (isEnemy && audioSource != null && hitSound != null)
+            {
+                audioSource.PlayOneShot(hitSound);
+            }
         }
     }
 }
