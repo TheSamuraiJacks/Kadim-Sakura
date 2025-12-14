03891571d4157d631408bc3ab2b55bb328cda555
Zehra Gürcan <111696861+ZehraGurcan@users.noreply.github.com>
2025-12-14 13:15:33 +0300

    sude sonrası push

--- Assets/Scripts/DayManaging.cs
+++ Assets/Scripts/DayManaging.cs
@@ -1,14 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Rendering;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class DayManaging : MonoBehaviour
 {
-    public string nextScene;
-
     public void UploadScene()
     {
-        SceneManager.LoadScene(nextScene);
+        int currentIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentIndex + 1);
     }
 }
