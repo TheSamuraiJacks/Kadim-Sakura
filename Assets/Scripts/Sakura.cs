faf96c39123c1850c5fc136a81200f936b23349c
Zehra Gürcan <111696861+ZehraGurcan@users.noreply.github.com>
2025-12-14 11:09:25 +0300

    sude push

---
+++ Assets/Scripts/Sakura.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sakura : MonoBehaviour
+{
+    float health = 100;
+    bool isAlive = true;
+
+    public void TakeDamage(float damage)
+    {
+        if (isAlive)
+        {
+            health -= damage;
+            if(health <= 0) isAlive = false;
+        }
+        if (!isAlive)
+        {
+            //Gameover ekraný
+        }
+
+    }
+}
